d3f4adda8a7f9ce9480f6f5be8c04b37e2d11b66
Konstantin Nekrasov <sknek@ya.ru>
2021-06-18 13:48:23 +0300

    -

--- windows/smartlock.bat
+++ windows/smartlock.bat
@@ -1,7 +1,10 @@
 @echo off
 
 cd ..
+
 FOR /L %%i IN (1,1,20) DO (
 	timeout 3 > NUL
-	python smartlock.py windows\smartlock.yaml -R
+	if NOT EXIST C:\STOP (
+		python smartlock.py windows\smartlock.yaml -R
+	)
 )
