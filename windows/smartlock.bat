3cfdcc986bed4e08609bb8a83d18590bb5fbabed
Konstantin Nekrasov <sknek@ya.ru>
2021-07-07 22:05:15 +0300

    -

--- windows/smartlock.bat
+++ windows/smartlock.bat
@@ -5,6 +5,6 @@ cd ..
 FOR /L %%i IN (1,1,20) DO (
 	timeout 3 > NUL
 	if NOT EXIST C:\smartlock.flags\STOP (
-		python smartlock.py windows\smartlock.yaml -R
+		type config.d\* windows\config.d\* | python smartlock.py -R
 	)
 )
